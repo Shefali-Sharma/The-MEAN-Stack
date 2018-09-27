9e1d5d0dd7651b97a662eed8f91e7efe42c21042
Shefali-Sharma <shefali92@gmail.com>
2018-09-26 20:44:27 -0400

    Adding CSS
    Adding CSS

--- mean-stack/src/app/posts/post-create/post-create.component.ts
+++ mean-stack/src/app/posts/post-create/post-create.component.ts
@@ -1,8 +1,9 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-post-create',
-  templateUrl: './post-create.component.html'
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent {
   enteredValue = '';
