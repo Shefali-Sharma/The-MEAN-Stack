4d6fdad18688abda3ed4900617a1e91403b4a3bd
Shefali-Sharma <shefali92@gmail.com>
2018-09-26 22:24:31 -0400

    Add CSS styling to HTML components
    Add CSS styling to HTML components

--- mean-stack/src/app/app.module.ts
+++ mean-stack/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {MatInputModule, MatCardModule} from '@angular/material';
+import {MatInputModule, MatCardModule, MatButtonModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
@@ -18,7 +18,8 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
     FormsModule,
     BrowserAnimationsModule,
     MatInputModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
