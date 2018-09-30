f29cd06703ade949f987352b034d1b8a81ac6f08
Shefali-Sharma <shefali92@gmail.com>
2018-09-30 02:04:28 -0400

    Binding Post - Lists and Creates
    Binding Post - Lists and Creates

--- mean-stack/src/app/posts/post-list/post-list.component.ts
+++ mean-stack/src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post-list',
@@ -6,9 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent {
-  posts = [
-    {title: 'First Post', content: 'This is first post\'s content'},
-    {title: 'Second Post', content: 'This is second post\'s content'},
-    {title: 'Third Post', content: 'This is third post\'s content'}
-  ];
+  // posts = [
+  //   {title: 'First Post', content: 'This is first post\'s content'},
+  //   {title: 'Second Post', content: 'This is second post\'s content'},
+  //   {title: 'Third Post', content: 'This is third post\'s content'}
+  // ];
+  @Input() posts = [];
 }
