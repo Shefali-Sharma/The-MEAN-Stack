96a2212b80cbc5e528f0c9f7edadf6e0805b006c
Shefali-Sharma <shefali92@gmail.com>
2018-09-30 02:23:34 -0400

    Converting to Form
    Converting to form

--- mean-stack/src/app/posts/post-create/post-create.component.ts
+++ mean-stack/src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 import { Post } from '../post.model';
 @Component({
   selector: 'app-post-create',
@@ -10,10 +12,13 @@ export class PostCreateComponent {
   enteredContent = '';
   @Output() postCreated = new EventEmitter<Post>();
 
-  onAddPost() {
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
     const post: Post = {
-      title: this.enteredTitle,
-      content: this.enteredContent
+      title: form.value.title,
+      content: form.value.content
     };
     this.postCreated.emit(post);
   }
