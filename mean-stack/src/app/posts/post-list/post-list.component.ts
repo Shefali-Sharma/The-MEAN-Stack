bc5b83c9a7ff4914a70e5d2eafb85c56483488b4
Shefali-Sharma <shefali92@gmail.com>
2018-10-01 16:02:12 -0400

    Calling GET Post
    Calling GET Post

--- mean-stack/src/app/posts/post-list/post-list.component.ts
+++ mean-stack/src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription} from 'rxjs';
+
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
 
@@ -7,13 +9,26 @@ import { PostsService } from '../posts.service';
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent {
+export class PostListComponent implements OnInit, OnDestroy {
   // posts = [
   //   {title: 'First Post', content: 'This is first post\'s content'},
   //   {title: 'Second Post', content: 'This is second post\'s content'},
   //   {title: 'Third Post', content: 'This is third post\'s content'}
   // ];
-  @Input() posts: Post[] = [];
+  posts: Post[] = [];
+  private postsSub: Subscription;
 
   constructor(public postsService: PostsService) {}
+
+  ngOnInit() {
+    this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListner()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
 }
