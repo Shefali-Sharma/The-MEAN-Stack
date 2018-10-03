bbc87701095af10f872e7726e9d79ad298097802
Shefali-Sharma <shefali92@gmail.com>
2018-10-03 13:25:58 -0400

    Updating app.use to allow communication over servers
    Updating app.use to allow communication over servers

--- mean-stack/src/app/posts/posts.service.ts
+++ mean-stack/src/app/posts/posts.service.ts
@@ -1,19 +1,28 @@
 import { Post } from './post.model';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
+
+  constructor(private http: HttpClient) {}
+
   getPosts() {
-    return [...this.posts];
+    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
+      .subscribe((postData) => {
+        this.posts = postData.posts;
+        this.postsUpdated.next([...this.posts]);
+      });
   }
   getPostUpdateListner() {
     return this.postsUpdated.asObservable();
   }
   addPost(title: string, content: string) {
-    const post: Post = {title: title, content: content};
+    const post: Post = {id: null, title: title, content: content};
     this.posts.push(post);
     this.postsUpdated.next([...this.posts]);
   }
