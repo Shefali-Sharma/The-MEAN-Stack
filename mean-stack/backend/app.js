f6484287ad9474ca78f58ddfe9da2a3d16f83271
Shefali-Sharma <shefali92@gmail.com>
2018-10-04 20:05:08 -0400

    Pushing Data to Atlas
    Pushing Data to Atlas

--- mean-stack/backend/app.js
+++ mean-stack/backend/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://shefali9222:o18Hk5Ow6mxiqTAA@cluster0-lqt5b.mongodb.net/node-angular?retryWrites=true")
+  .then(() => {
+    console.log('Connected to database!');
+  })
+  .catch(() => {
+    console.log('Connection failed!');
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,7 +35,7 @@ app.post("/api/posts", (req, res, next) => {
     title: req.body.title,
     content: req.body.content
   });
-  console.log(post);
+  post.save();
   res.status(201).json({
     message: 'Post added successfully'
   });
